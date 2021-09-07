f1d795a3670f9b4233016734f5e1e360eeb8f433
Jahir Garcia <jahirgarcia66@gmail.com>
2021-09-06 23:40:32 -0600

    feat(upload): se puede subir archvios

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const multer = require('multer');
 const path = require('path');
 const app = express();
+
+const upload = multer({
+  dest: path.resolve(__dirname, 'uploads')
+});
+
+const handleUploadError = upload => (req, res, next) => {
+  upload(req, res, err => {
+    if(err instanceof multer.MulterError)
+      res.sendStatus(400);
+    else if(err)
+      res.sendStatus(500);
+    else
+      next();
+  });
+}
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -12,8 +28,9 @@ app.get('/', (req, res) => {
 
 app.get('/upload', (req, res) => res.render('upload'));
 
-app.post('/upload', (req, res) => {
-  res.end('done!!!');
-})
+app.post('/upload', handleUploadError(upload.single('file')), (req, res) => {
+  console.log(req.file)
+  res.redirect('/');
+});
 
 module.exports = app;
