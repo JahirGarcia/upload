25f3e1bd8e569cef5c92c2bbc27bc40c458899d4
Jahir Garcia <jahirgarcia66@gmail.com>
2021-09-07 21:38:24 -0600

    feat(upload): se guarda la informacion de los archivos y se pueden descargar

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
-const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const express = require('express');
+const { resolve } = require('path');
+const { createHash } = require('crypto');
+const { constants: { R_OK, W_OK }} = require('fs');
+const { access, readFile, writeFile } = require('fs/promises');
+
 const app = express();
 
+const uploadPath = resolve(__dirname, 'uploads');
 const upload = multer({
-  dest: path.resolve(__dirname, 'uploads')
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => cb(null, uploadPath),
+    filename: (req, file, cb) => {
+      const originalNameHash = createHash('md5');
+      originalNameHash.update(file.originalname);
+      const hashedOriginalName = originalNameHash.digest('hex');
+
+      const filenameHash = createHash('md5');
+      filenameHash.update(`${hashedOriginalName}@${file.filename}`);
+      const hashedFilename = filenameHash.digest('hex');
+
+      cb(null, hashedFilename);
+    }
+  })
 });
 
 const handleUploadError = upload => (req, res, next) => {
@@ -18,19 +36,50 @@ const handleUploadError = upload => (req, res, next) => {
   });
 }
 
-app.set('views', path.resolve(__dirname, 'views'));
+app.set('views', resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  const files = [];
-  res.render('index', { files });
+app.get('/', async (req, res) => {
+  const path = resolve(__dirname, 'db.json');
+  try {
+    await access(path, R_OK | W_OK);
+    const data = await readFile(path);
+    const files = JSON.parse(data);
+    // const realFilenames = files.map(file => {
+
+    // });
+    res.render('index', { files });
+  } catch(err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.get('/upload', (req, res) => res.render('upload'));
 
-app.post('/upload', handleUploadError(upload.single('file')), (req, res) => {
-  console.log(req.file)
-  res.redirect('/');
+app.post('/upload', handleUploadError(upload.single('file')), async (req, res) => {
+  const path = resolve(__dirname, 'db.json');
+  try {
+    await access(path, R_OK | W_OK);
+    const data = await readFile(path);
+    const files = JSON.parse(data);
+
+    const { filename, mimetype } = req.file;
+    files.push({
+      name: filename, 
+      type: mimetype 
+    });
+
+    await writeFile(path, JSON.stringify(files));
+    res.redirect('/');
+  } catch(err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/download/:file', (req, res) => {
+  res.sendFile(resolve(uploadPath, req.params.file));
 });
 
 module.exports = app;
